1dec721675ffb522575092c7f8f6d74f4b2d0130
Salar Khan <salarkhan586@gmail.com>
2024-11-06 09:50:13 +0530

    wrapped client caraousel component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import CaseStudies from "./components/CaseStudies";
+import ClientCarousel from "./components/ClientCarousel";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
 import Quote from "./components/Quote";
@@ -5,11 +7,13 @@ import Quote from "./components/Quote";
 function App() {
   return (
     <div>
-      <div className="flex flex-col    lg:flex-row lg:justify-around lg:items-center  mt-10 mb-10 ">
+      {/* <ClientCarousel /> */}
+      <CaseStudies />
+      {/* <div className="flex flex-col    lg:flex-row lg:justify-around lg:items-center  mt-10 mb-10 ">
         <Faq />
         <Quote />
-      </div>
-      <Footer />{" "}
+      </div> */}
+      {/* <Footer />{" "} */}
     </div>
   );
 }
--- src/components/ClientCarousel.js
+++ src/components/ClientCarousel.js
@@ -1,7 +1,105 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/pagination";
 const ClientCarousel = () => {
-  return <div></div>;
+  const cardData = [
+    {
+      id: 1,
+      frame: "./client-1.png",
+      avatar: "./client-1-avatar.png",
+      name: "Sara Thomas",
+      designation: "Co Founder urh",
+      content:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies ",
+    },
+    {
+      id: 2,
+      frame: "./client-1.png",
+      avatar: "./client-1-avatar.png",
+      name: "Sara Thomas",
+      designation: "Co Founder urh",
+      content:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies ",
+    },
+    {
+      id: 3,
+      frame: "./client-1.png",
+      avatar: "./client-1-avatar.png",
+      name: "Sara Thomas",
+      designation: "Co Founder urh",
+      content:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies ",
+    },
+    {
+      id: 4,
+      frame: "./client-1.png",
+      avatar: "./client-1-avatar.png",
+      name: "Sara Thomas",
+      designation: "Co Founder urh",
+      content:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pLorem ipsum dolor sit amet, consectetuer adipiscing elit. AenLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies ",
+    },
+  ];
+  return (
+    <div className="font-outfit">
+      <h2 className="font-bold text-4xl text-center mb-5">
+        Clients Testimonial
+      </h2>
+      <Swiper
+        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        style={{
+          "--swiper-pagination-color": "#26b5a1",
+          "--swiper-pagination-bullet-inactive-color": "#d9d9d9",
+          "--swiper-pagination-bullet-inactive-opacity": "1",
+          "--swiper-pagination-bullet-size": "16px",
+          "--swiper-pagination-bullet-horizontal-gap": "6px",
+          "--swiper-pagination-bottom": "0px",
+        }}
+      >
+        {cardData.map((card) => {
+          return (
+            <SwiperSlide key={card.id} className=" lg:pb-10 px-5 lg:px-0 pb-5 ">
+              <div className="flex flex-col lg:flex-row justify-around items-center  ">
+                <div id="frame">
+                  <img src={card.frame} alt="Person Image" />
+                </div>
+                <div className="w-full  lg:w-[600px]">
+                  <div className="flex flex-col ">
+                    <img src="./quotes.png" width={45} height={45} />
+                    <p className="mt-4 text-base font-normal text-[#494949]">
+                      {card.content}
+                    </p>
+                    <div className="flex  items-center mt-4">
+                      <img src={card.avatar} />
+                      <div className="flex flex-col mx-2">
+                        <p className="font-semibold text-xl text-textBlack">
+                          {card.name}
+                        </p>
+                        <p className="font-normal text-2xl text-[#494949]">
+                          {card.designation}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default ClientCarousel;
